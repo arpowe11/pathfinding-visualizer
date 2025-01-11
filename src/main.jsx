81a572c162503ad24713de1b6c4c171f9c44cc6d
Alex Powell <apowell@sunsigndesigns.com>
2025-01-11 00:42:57 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import MazeGrid from './MazeGrid.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <MazeGrid />
+  </StrictMode>,
+)
